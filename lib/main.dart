87e9c61826ad5b4ce7791955571bff5533a0ec25
Pavan Hegde <pavanhegde81@gmail.com>
2023-09-19 14:10:42 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'getting_live_location.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: LiveLocationMap()
+    );
+  }
+}
