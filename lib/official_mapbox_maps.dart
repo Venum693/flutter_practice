4999934f04293cd712264d9c3ff1f9dc53d2f39d
Pavan Hegde <pavanhegde81@gmail.com>
2023-09-27 15:20:06 +0530

    added new code for flutter practice.dart

--- lib/official_mapbox_maps.dart
+++ lib/official_mapbox_maps.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class FullMap extends StatefulWidget {
   const FullMap();
@@ -11,45 +12,50 @@ class FullMap extends StatefulWidget {
 }
 
 class FullMapState extends State<FullMap> {
-  late MapboxMap mapboxMapcontroller;
+   MapboxMap? mapboxMapcontroller;
 
   _onMapCreated(MapboxMap mapboxMap) async {
-    //mapboxMapcontroller = mapboxMap;
-    // mapboxMap.location.updateSettings(
-    //   LocationComponentSettings(
-    //     locationPuck: LocationPuck(
-    //      locationPuck3D: LocationPuck3D(
-    //        modelUri:"https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF-Embedded/Duck.gltf"
-    //      )
-    //     ),
-    //   )
-    // );
     mapboxMapcontroller = mapboxMap;
-    await mapboxMapcontroller.annotations.createPointAnnotationManager().then((value) async{
-      final ByteData bytes = await rootBundle.load('assets/images/Group 71.png');
-      final Uint8List list = bytes.buffer.asUint8List();
-      var options = <PointAnnotationOptions>[];
-      for (var i = 0; i < 5; i++){
-        options.add(PointAnnotationOptions(
-            geometry: Point(coordinates: Position(77.6408,12.9784)).toJson(), image: list,),);
-      }
-      //pointAnnotationManager?. = value;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapWidget(resourceOptions: ResourceOptions(
-          accessToken: 'pk.eyJ1IjoiaGVscHltb3RvIiwiYSI6ImNsamNscHVuejAyOXAzZG1vNXppYnM1NzkifQ.BB9fpPJb9eDpRJkWwkRHXA'
-      ),
-      onMapCreated: _onMapCreated,
-        key: ValueKey('map_widget'),
-        // cameraOptions: CameraOptions(
-        //   center: Point(coordinates: Position(77.6408,12.9784)).toJson(),
-        //   zoom: 20.0,
-        // ),
-      ),
+      body: Stack(
+        children: [
+          MapWidget(resourceOptions: ResourceOptions(
+              accessToken: 'pk.eyJ1IjoiaGVscHltb3RvIiwiYSI6ImNsamNscHVuejAyOXAzZG1vNXppYnM1NzkifQ.BB9fpPJb9eDpRJkWwkRHXA'
+          ),
+            onMapCreated: _onMapCreated,
+            key: ValueKey('map_widget'),
+            // cameraOptions: CameraOptions(
+            //   center: Point(coordinates: Position(77.6408,12.9784)).toJson(),
+            //   zoom: 20.0,
+            // ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 50,
+              color: Colors.black,
+              child: TextButton(onPressed: () async {
+                var status = await Permission.locationWhenInUse.request();
+                print("Location granted : $status");
+                mapboxMapcontroller?.easeTo(CameraOptions(
+                  zoom: 17,
+                  center: Point(coordinates: Position(77.6408,12.9784)).toJson(),
+                ),
+                    MapAnimationOptions(duration: 2000, startDelay: 0));
+                  // mapboxMapcontroller?.location
+                  //     .updateSettings(LocationComponentSettings(enabled: true,));
+                  // mapboxMapcontroller?.setCamera(CameraOptions(
+                  //   center: Point(coordinates: Position(1.0,2.0)).toJson(),
+                  //   zoom: 16.0,
+                  // ));
+            }, child: Text('Locate Me'),),),
+          )
+        ],
+      )
     );
   }
 }
